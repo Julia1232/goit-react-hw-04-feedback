7fcacd0432ffd670da786a795934511a75f07b78
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2023-03-28 16:09:19 +0300

    second

--- src/components/FeedbackButton/FeedbackButton.jsx
+++ src/components/FeedbackButton/FeedbackButton.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import css from './FeedbackButton.module.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FeedbackButton.module.css';
 
 const FeedbackButton = ({ options, onIncrement }) => {
-    return (
-        <>
-            {options.map((option) => (
-                <button key={option} type="button" name={option} onClick={onIncrement} className={css.FeedbackButton}>
-                    {option}
-                </button>
-            ))}
-        </>
-    )
+  return (
+    <>
+      {options.map(option => (
+        <button
+          key={option}
+          type="button"
+          name={option}
+          onClick={onIncrement}
+          className={css.FeedbackButton}
+        >
+          {option}
+        </button>
+      ))}
+    </>
+  );
 };
 FeedbackButton.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onIncrement: PropTypes.func.isRequired,
-}
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onIncrement: PropTypes.func.isRequired,
+};
 export default FeedbackButton;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-import css from './Statistics.module.css'
+import css from './Statistics.module.css';
 
-export default function Statistics({good, neutral, bad, total, positiRound }) {
+export default function Statistics({ good, neutral, bad, total, positiRound }) {
   return (
     <div className={css.statisticsName}>
-            <p>Good: {good}</p>
-            <p>Neutral: {neutral}</p>
-            <p>Bad: {bad}</p>
-            <p>Total: {total}</p>
-            <p>Positive feedback: {positiRound.toFixed(1)} %</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive feedback: {positiRound.toFixed(1)} %</p>
     </div>
-  )
+  );
 }
 Statistics.propTypes = {
   good: PropTypes.number,
   neutral: PropTypes.number,
   bad: PropTypes.number,
-  total:PropTypes.func,
+  total: PropTypes.func,
   positiRound: PropTypes.func,
 };
